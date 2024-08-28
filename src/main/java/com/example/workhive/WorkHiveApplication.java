7445efefdfd16c0f58565e97ba664ce6d176d3c2
Hyo <hyoseongkim1995@gmail.com>
2024-08-28 14:11:34 +0900

    first init

---
+++ src/main/java/com/example/workhive/WorkHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.workhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workhive/WorkHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
