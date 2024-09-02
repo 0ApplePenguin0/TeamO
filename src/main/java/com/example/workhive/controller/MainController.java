0f095244184b4914ccbc260823396f3d11ae2fd7
Hyo <hyoseongkim1995@gmail.com>
2024-09-02 15:16:54 +0900

    로그인 회원가입

---
+++ src/main/java/com/example/workhive/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.workhive.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main")
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("board")
+    public String board(){
+        return "main/mainboard";
+    };
+}
