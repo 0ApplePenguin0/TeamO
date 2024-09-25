a3b26e2269430ea92c6137b608d2bae7b674b0b4
ApplePenguin <choip1115@naver.com>
2024-09-25 14:47:46 +0900

    application.properties 에서 spring.jpa.hibernate.ddl-auto=update 의 update를 validate로 변경, MemoEntity 에서 title 을 삭제

---
+++ src/main/java/com/example/workhive/domain/entity/CategoryEntity.java
@@ -0,0 +1,28 @@
+package com.example.workhive.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;  // 카테고리 ID (일련번호)
+
+    @Column(name = "category_name", length = 50)
+    private String categoryName;  // 카테고리 이름 (null 가능)
+
+    @Column(name = "color", nullable = false, length = 255)
+    private String color;  // 카테고리 색상 (null 불가)
+
+}
