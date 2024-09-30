7071b868c7ae17dc567b0bbf019b80a6cfef3c02
lawf16-admin <lazywolfe16@gmail.com>
2024-09-30 11:25:04 +0900

    관리자 페이지 추가

--- src/main/resources/static/js/mainJs/mainboard.js
+++ src/main/resources/static/js/mainJs/mainboard.js
@@ -6,3 +6,13 @@ document.getElementById('approvalMenu').addEventListener('click', function () {
         submenu.style.display = 'none';
     }
 });
+
+
+    document.getElementById('adminpage').addEventListener('click', function () {
+        let submenu2 = document.getElementById('adminpageSubmenu');
+        if (submenu2.style.display === 'none' || submenu2.style.display === '') {
+            submenu2.style.display = 'block';
+        } else {
+            submenu2.style.display = 'none';
+        }
+});
